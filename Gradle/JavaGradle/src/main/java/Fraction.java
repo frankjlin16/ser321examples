d90db840d74f3b0361b3bdbdbf5eb8829c3364a1
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-05-23 20:24:25 -0700

    Update Fraction.main

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -5,6 +5,7 @@
  *
  * Ser321 Foundations of Distributed Applications
  * see http://pooh.poly.asu.edu/Ser321
+ * 
  * @author Tim Lindquist Tim.Lindquist@asu.edu
  *         Software Engineering, CIDSE, IAFSE, ASU Poly
  * @version January 2020
@@ -13,19 +14,19 @@ public class Fraction {
 
    private int numerator, denominator;
 
-   public Fraction(){
+   public Fraction() {
       numerator = denominator = 0;
    }
 
    public void print() {
-    System.out.print(numerator + "/" + denominator );
+      System.out.print(numerator + "/" + denominator);
    }
 
-   public void setNumerator (int n ){
+   public void setNumerator(int n) {
       numerator = n;
    }
 
-   public void setDenominator (int d) {
+   public void setDenominator(int d) {
       denominator = d;
    }
 
@@ -37,24 +38,28 @@ public class Fraction {
       return numerator;
    }
 
-   public static void main (String args[]) {
+   public static void main(String args[]) {
       try {
          // create a new instance
          // Fraction *frac = [[Fraction alloc] init];
          Fraction frac = new Fraction();
 
-         // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+         if (args.length == 2) {
+            frac.setNumerator(Integer.parseInt(args[0]));
+            frac.setDenominator(Integer.parseInt(args[1]));
+         } else {
+            // set the values
+            frac.setNumerator(1);
+            frac.setDenominator(3);
+         }
 
          // print it
          System.out.print("The fraction is: ");
          frac.print();
          System.out.println("");
 
-      }catch(Exception e) {
+      } catch (Exception e) {
          e.printStackTrace();
       }
    }
 }
-
